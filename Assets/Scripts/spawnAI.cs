056786e2ec8d2999630cbfc444704a44362a810e
Ficko <filip-f@hotmail.com>
2019-02-26 15:19:08 +0100

    spawn ai

---
+++ Assets/Scripts/spawnAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnAI : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnDelay = 10;
+    public GameObject Spawnpoint;
+
+   
+    public void TaskOnClick()
+    {
+        spawn();
+    }
+
+    void spawn()
+    {
+        prefab.SetActive(true);
+        Instantiate(prefab, Spawnpoint.transform.position, Spawnpoint.transform.rotation);
+    }
+
+}
