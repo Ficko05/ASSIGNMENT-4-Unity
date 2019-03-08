d4265998c7e54929f1a2e00bbf8502c9dcc569b7
Steven <Steven-ptkhuu@hotmail.com>
2019-03-08 15:20:14 +0100

    dont stop agent turner

---
+++ Assets/Scripts/dontBlockAgentTurner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontBlockAgentTurner : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "turret")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
