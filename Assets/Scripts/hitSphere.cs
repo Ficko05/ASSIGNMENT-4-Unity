ffdbbe23fe0c3f44b022426fcaac96a09327d027
Steven <Steven-ptkhuu@hotmail.com>
2019-03-08 13:05:02 +0100

    hitShpere Script and HealthScript is working

---
+++ Assets/Scripts/hitSphere.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitSphere : MonoBehaviour
+{
+
+    public float radius = 5;
+    //public spawnAI script;
+    public float cooldownTimer = 2f;
+    bool cooldown = false;
+    public float resetTimer = 2f;
+    void Update()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius);
+        if (cooldown == false)
+        {
+            foreach (Collider hit in hits)
+            {
+                if (hit.tag == "mob")
+                {
+                    if (cooldown == false)
+                    {
+                        cooldown = true;
+                        cooldownTimer = resetTimer;
+                        if (hit.transform.gameObject.GetComponent<healthScript>().reduceHealth())
+                        {
+                            Destroy(hit.gameObject);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (cooldown == true)
+        {
+            cooldownTimer = cooldownTimer - Time.deltaTime;
+            print(cooldownTimer);
+            if (cooldownTimer <= 0)
+            {
+                cooldown = false;
+            }
+        }
+    }
+
+}
